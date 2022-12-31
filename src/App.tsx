f3f04c752cecc4b10905500d09113ca4666af71c
Edwin Olivo Garcia <17003685@uan.edu.mx>
2022-12-31 09:24:18 -0700

    svg file has its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import Checkbox from "./components/Checkbox";
+import CopyFile from "./components/CopyFile";
 import Slider from "./components/Slider";
 import copyToClipboard from "./utils/copy-clipboard";
 import { generatePassword } from "./utils/pass-generator";
@@ -33,26 +34,7 @@ function App() {
         </div>
         <div>
           <a onClick={copyToClipboard} className="copy-btn">
-            <svg
-              fill="currentColor"
-              viewBox="0 0 48 48"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <title>copy-file</title>
-              <g id="Layer_2" data-name="Layer 2">
-                <g id="invisible_box" data-name="invisible box">
-                  <rect width="48" height="48" fill="none" />
-                </g>
-                <g id="icons_Q2" data-name="icons Q2">
-                  <g>
-                    <path d="M18,30H14a2,2,0,0,1,0-4h4a2,2,0,0,1,0,4Z" />
-                    <path d="M26,22H14a2,2,0,0,1,0-4H26a2,2,0,0,1,0,4Z" />
-                    <path d="M26,14H14a2,2,0,0,1,0-4H26a2,2,0,0,1,0,4Z" />
-                    <path d="M42,8H36V4a2,2,0,0,0-2-2H6A2,2,0,0,0,4,4V36a2,2,0,0,0,2,2h4v6a2,2,0,0,0,2,2H42a2,2,0,0,0,2-2V10A2,2,0,0,0,42,8ZM8,6H32V34H8ZM40,42H14V38H34a2,2,0,0,0,2-2V12h4Z" />
-                  </g>
-                </g>
-              </g>
-            </svg>
+            <CopyFile />
           </a>
         </div>
       </div>
